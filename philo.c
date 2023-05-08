ab280213d4cf79a2f5d21b3145a59491171e6d3a
youssefbourais0310 <youssef.bourais0310@gmail.com>
2023-05-08 13:05:05 +0100

    modifiead the code and deleted some unused mutex (for printing) , start visualizing the dinig philo problem 2

--- philo.c
+++ philo.c
@@ -12,10 +12,6 @@
 
 # include "philo.h"
 
-// ◦ timestamp_in_ms X has taken a fork 
-// ◦ timestamp_in_ms X is eating
-// ◦ timestamp_in_ms X is sleeping
-// ◦ timestamp_in_ms X is thinking
 // ◦ timestamp_in_ms X died
 
 // number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
